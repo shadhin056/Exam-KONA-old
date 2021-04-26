4a686ee82d1876ee5875bd096cc344b4480a320e
Moniruzzaman ShadhiN <shadhinemail@gmail.com>
2021-04-27 01:10:06 +0600

    validation working well for reg

--- app/src/main/java/com/example/konamoniruzzaman/RegistrationActivity.kt
+++ app/src/main/java/com/example/konamoniruzzaman/RegistrationActivity.kt
@@ -9,17 +9,30 @@ import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import cn.pedant.SweetAlert.SweetAlertDialog
 import kotlinx.android.synthetic.main.activity_registration.*
+import java.text.ParseException
+import java.text.SimpleDateFormat
 import java.util.*
+import java.util.regex.Pattern
 
 
 class RegistrationActivity : AppCompatActivity() {
+    private lateinit var pDialog: SweetAlertDialog
     internal lateinit var picker: DatePickerDialog
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
         supportActionBar!!.setDisplayShowHomeEnabled(true)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        pDialog = SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE)
+        pDialog.titleText = "Loading"
+        pDialog.setCancelable(false)
+        // pDialog.show()
+
+
         spinnerLoad()
         spinnerOnChange()
         buttonAction()
@@ -37,15 +50,15 @@ class RegistrationActivity : AppCompatActivity() {
                     spPostOffice.setSelection(0)
                     spThane.setSelection(0)
                     spDistrict.setSelection(0)
-                }else if (position == 1){
+                } else if (position == 1) {
                     spPostOffice.setSelection(1)
                     spThane.setSelection(1)
                     spDistrict.setSelection(1)
-                }else if (position == 2){
+                } else if (position == 2) {
                     spPostOffice.setSelection(2)
                     spThane.setSelection(2)
                     spDistrict.setSelection(2)
-                }else if (position == 3){
+                } else if (position == 3) {
                     spPostOffice.setSelection(3)
                     spThane.setSelection(3)
                     spDistrict.setSelection(3)
@@ -58,8 +71,60 @@ class RegistrationActivity : AppCompatActivity() {
         })
     }
 
+    fun ageCheck(): Boolean {
+
+        // check if the age is >= 1
+        try {
+            val calendarBirthday = Calendar.getInstance()
+            val calendarToday = Calendar.getInstance()
+            calendarBirthday.time =
+                SimpleDateFormat("dd/MM/yyyy", Locale.US).parse(edtBirthday.text.toString())
+            val yearOfToday = calendarToday[Calendar.YEAR]
+            val yearOfBirthday = calendarBirthday[Calendar.YEAR]
+            if (yearOfToday - yearOfBirthday >= 18) {
+                return false
+            }
+        } catch (e: ParseException) {
+            e.printStackTrace()
+        }
+        return true
+    }
+
+
     private fun buttonAction() {
-        ibcalender_btn.setOnClickListener{
+
+
+        val anyNumberCheck = ".*\\d.*"
+
+
+        btnSubmit.setOnClickListener {
+            if (edtCustomerName.text.toString().length < 2) {
+                SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
+                    .setTitleText("Warning")
+                    .setContentText("More than 2 character needed for Customer Name")
+                    .show()
+            } else if (Pattern.matches(anyNumberCheck, edtCustomerName.text.toString())) {
+                SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
+                    .setTitleText("Warning")
+                    .setContentText("Number Not Allow in Customer Name")
+                    .show()
+            }  else if (ageCheck()) {
+                SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
+                    .setTitleText("Warning")
+                    .setContentText("Age Less than 18 years")
+                    .show()
+            } else if (edtCustomerPhone.text.toString().length != 11) {
+                SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
+                    .setTitleText("Warning")
+                    .setContentText("Number Can't more or less than 11 Digit")
+                    .show()
+            }
+
+
+        }
+
+
+        ibcalender_btn.setOnClickListener {
             val cldr = Calendar.getInstance()
             val day = cldr.get(Calendar.DAY_OF_MONTH)
             val month = cldr.get(Calendar.MONTH)
@@ -77,8 +142,9 @@ class RegistrationActivity : AppCompatActivity() {
 
 
         }
-          
+
     }
+
     private fun spinnerLoad() {
 
         //postal code
@@ -115,8 +181,9 @@ class RegistrationActivity : AppCompatActivity() {
         adapterForDistrict.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spDistrict.setAdapter(adapterForDistrict)
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
+        when (item.itemId) {
             android.R.id.home -> {
                 val intent: Intent = Intent(
                     this,
@@ -129,3 +196,4 @@ class RegistrationActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 }
+
