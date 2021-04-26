01ea153058dbbbc884afff0608b644c8cdaf66e5
Moniruzzaman ShadhiN <shadhinemail@gmail.com>
2021-04-26 23:33:29 +0600

    View created for registration and view customer

---
+++ app/src/main/java/com/example/konamoniruzzaman/ViewCustomer.kt
@@ -0,0 +1,11 @@
+package com.example.konamoniruzzaman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewCustomer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_customer)
+    }
+}
